8803e614a87926560d86e41b0fcbd8cb3a9b7f67
CaioAndreta <caioandreta@gmail.com>
2022-06-13 07:24:32 -0300

    13/06/22 07h24

---
+++ Challenges (HTML/CSS/JS)/Progress Steps/script.js
@@ -0,0 +1,36 @@
+let width = 0, contador = 1;
+const progress = document.getElementById('progress-value');
+const progressBar = document.querySelector('.progress')
+const steps = document.querySelectorAll('.step');
+const backButton = document.getElementById('back');
+const nextButton = document.getElementById('next');
+
+nextButton.addEventListener('click', () => {
+    //progress bar
+    width += progressBar.clientWidth / (steps.length - 1);
+    progress.style.width = width + 'px';
+
+    //numbers
+    steps[contador].classList.add('active')
+    contador++
+})
+
+backButton.addEventListener('click', () => {
+    //progress bar
+    width -= progressBar.clientWidth / (steps.length - 1);
+    progress.style.width = width + 'px';
+
+    //numbers
+    contador--
+    steps[contador].classList.remove('active')
+})
+
+if (contador > 3) {
+    nextButton.setAttribute('disabled', 'disabled');
+    backButton.removeAttribute('disabled', 'disabled');
+}
+
+if (contador < 3) {
+    backButton.setAttribute('disabled', 'disabled');
+    nextButton.setAttribute('disabled', 'disabled');
+}
